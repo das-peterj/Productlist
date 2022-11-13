e1fc0f2836b05d0ac2533830db09a4f48b37fb21
Peter Jorgensen <dasbomber.jorgensen@gmail.com>
2022-11-13 22:04:43 +0100

    Add project files.

---
+++ Checkpoint1/Program.cs
@@ -0,0 +1,76 @@
+﻿string[] productNames = new string[100];
+int index = 0;
+
+while (true)
+{
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("Please input the productnumber: ");
+    string input = Console.ReadLine();
+
+    if (input.ToLower().Trim() == "exit")
+    {
+        break;
+    }
+
+    string productLetter = "";
+    string checkProductNumber = "";
+    int productNumber = 0;
+    while (true)
+    {
+        if (input.Contains("-"))
+        {
+            productLetter = input.Substring(0, input.IndexOf("-"));
+            checkProductNumber = input.Substring(input.IndexOf("-") + 1);
+            if (checkProductNumber.Any(char.IsLetter))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Only numerical values are accepted right of the dash.");
+                break;   
+            }
+            productNumber = Int32.Parse(checkProductNumber);
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Missing dash from product name. Try again.");
+            break;
+        }
+
+        if (productNumber < 200) {
+            Console.ForegroundColor = ConsoleColor.Red; 
+            Console.WriteLine("The numerical value can't be lower than 200."); 
+            break; 
+        }
+        else if (productNumber > 500) {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("The numerical value can't be higher than 500."); 
+            break; 
+        }
+        else if (productLetter.Any(char.IsDigit)) {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Numerical values not accepted left of the dash"); 
+            break; 
+        }
+        else
+        {
+            productNames[index] = input;
+            index++;
+            break;
+        }
+    }
+}
+Console.WriteLine("");
+Console.WriteLine("");
+
+Array.Resize(ref productNames, index);
+Array.Sort(productNames);
+
+Console.WriteLine("You wrote the following product names (sorted alpabetically)");
+
+foreach(string productName in productNames)
+{
+    Console.WriteLine(productName);
+}
+
+
+
---
+++ Checkpoint1/README.md
@@ -0,0 +1,9 @@
+# Produktlista 
+
+## Intro 
+
+Du ska skriva en console-app som frågar efter produkter. 
+
+Efter att användaren har skrivit *exit* så visas alla (korrekt) inmatade produkter. 
+
+Den första nivån av programmet är i nivå med vad du jobbat med första veckan.
